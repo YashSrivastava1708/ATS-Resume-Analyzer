a71e00137de373c95e5967f64f912d671ab385f4
yash srivastava <yashsrivastava@yashs-MacBook-Air.local>
2026-06-18 03:11:58 +0530

    Add PDF text extraction

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 # from fastapi import FastAPI
+from pypdf import PdfReader
 from fastapi import FastAPI, UploadFile
 app = FastAPI()
 
@@ -32,13 +33,39 @@ def version():
 #     }
 
 #more ehanced upload API:
+
+
+# @app.post("/upload-resume")
+# async def upload_resume(file: UploadFile):
+
+#     content = await file.read()
+
+#     return {
+#         "filename": file.filename,
+#         "content_type": file.content_type,
+#         "size_in_bytes": len(content)
+#     }
+
+
+
+# upload resume api with pdf reading feature
+
 @app.post("/upload-resume")
 async def upload_resume(file: UploadFile):
 
     content = await file.read()
 
+    with open("temp_resume.pdf", "wb") as f:
+        f.write(content)
+
+    reader = PdfReader("temp_resume.pdf")
+
+    extracted_text = ""
+
+    for page in reader.pages:
+        extracted_text += page.extract_text()
+
     return {
         "filename": file.filename,
-        "content_type": file.content_type,
-        "size_in_bytes": len(content)
+        "text": extracted_text[:1000]
     }
